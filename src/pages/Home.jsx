23d061e01d873ef9d1d4fc02c58bed8e5a70d0fe
Famelgas <filipe.damendes@gmail.com>
2023-11-28 16:02:41 +0000

    resolver issues com as imagens dragable

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,38 @@ import React, { Component } from "react";
 import Menu from '../components/Menu'
 import home from '../styles/home.module.css';
 import navigation from '../information/navigation.json';
+import Draggable from 'react-draggable';
+
+const imagesHomepage = [
+  {
+    "id": "1",
+    "link": "query from wordpress 1"
+  },
+  {
+    "id": "2",
+    "link": "query from wordpress 2"
+  },
+  {
+    "id": "3",
+    "link": "query from wordpress 3"
+  },
+  {
+    "id": "4",
+    "link": "query from wordpress 4"
+  }
+];
 
 export default class Home extends Component {
-
   render() {
-
     return (
       <div className={home.page}>
-
         <div>
-          <div className={home.imageHome}>icon dragable in the background</div>
+          {imagesHomepage.map((image, index) => (
+            <Draggable>
+              <img key={home.image} src={image.link} alt={`Image ${index + 1}`} />
+            </Draggable>
+          ))}
         </div>
-
-
       </div>
     );
   }
